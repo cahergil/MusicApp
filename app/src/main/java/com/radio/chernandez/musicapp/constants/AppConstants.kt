ef73787269d3015f9bd683d74b441034d4333ff3
Carlos Hernández <chernandezgil@gmail.com>
2016-11-15 20:46:37 +0100

    Initial commit

---
+++ app/src/main/java/com/radio/chernandez/musicapp/constants/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.radio.chernandez.musicapp.constants
+
+/**
+ * Created by Carlos on 15/11/2016.
+ */
+class AppConstants {
+    companion object {
+        val api_key = ""
+    }
+}
---
+++ app/src/main/java/com/radio/chernandez/musicapp/data/Prueba.kt
@@ -0,0 +1,7 @@
+package com.radio.chernandez.musicapp.data
+
+/**
+ * Created by Carlos on 15/11/2016.
+ */
+class Prueba {
+}
